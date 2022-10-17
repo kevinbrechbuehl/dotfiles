856aa580e48026127abf4836ac928a5dfabb85b8
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2022-10-17 22:16:00 +0200

    Add Visual Studio Code settings

--- update.ps1
+++ update.ps1
@@ -2,6 +2,11 @@
 New-Item -ItemType Directory -Force -Path ".\git\"
 Copy-Item "$env:USERPROFILE\.gitconfig" -Destination ".\git\"
 
+# Visual Studio Code
+New-Item -ItemType Directory -Force -Path ".\vscode\"
+Copy-Item "$env:APPDATA\Code\User\settings.json" -Destination ".\vscode\"
+code --list-extensions > ".\vscode\extensions.txt"
+
 # Windows Terminal
 New-Item -ItemType Directory -Force -Path ".\windows-terminal\"
 Copy-Item "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Destination ".\windows-terminal\"
