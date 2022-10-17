ca9db16494dce0c04d6b5b6709197345e670c0b3
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2022-10-17 22:30:08 +0200

    Add PowerShell profile config

--- update.ps1
+++ update.ps1
@@ -2,6 +2,10 @@
 New-Item -ItemType Directory -Force -Path ".\git\"
 Copy-Item "$env:USERPROFILE\.gitconfig" -Destination ".\git\"
 
+# PowerShell
+New-Item -ItemType Directory -Force -Path ".\powershell\"
+Copy-Item "$env:USERPROFILE\Documents\PowerShell\Microsoft.PowerShell_profile.ps1" -Destination ".\powershell\"
+
 # Visual Studio Code
 New-Item -ItemType Directory -Force -Path ".\vscode\"
 Copy-Item "$env:APPDATA\Code\User\settings.json" -Destination ".\vscode\"
