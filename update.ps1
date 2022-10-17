e75f601b929640c97babc5301cc408dc458fc697
Kevin Brechbühl <mail@kevinbrechbuehl.ch>
2022-10-17 22:09:37 +0200

    Add config for Windows Terminal

--- update.ps1
+++ update.ps1
@@ -1,3 +1,7 @@
 # Git
 New-Item -ItemType Directory -Force -Path ".\git\"
 Copy-Item "$env:USERPROFILE\.gitconfig" -Destination ".\git\"
+
+# Windows Terminal
+New-Item -ItemType Directory -Force -Path ".\windows-terminal\"
+Copy-Item "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Destination ".\windows-terminal\"
